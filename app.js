92dacee3b1953f58c2969a951dc8e5432e2dd3e6
Kevin Smith <kevin@kvnsmith.com>
2018-05-19 11:49:59 -0600

    server app

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const http = require('http');
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end("Hello, World!\n");
-}).listen(process.env.PORT);
+// http.createServer(function(request, response) {
+//   response.writeHead(200, {'Content-Type': 'text/plain'});
+//   response.end("Hello, World!\n");
+// }).listen(process.env.PORT);
 
-console.log('App is running...');
+// console.log('App is running...');
 
 var express = require("express");
 var bodyParser = require("body-parser");
