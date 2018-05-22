101502d9698f22fb88cb12b4ef14d27548e3c0cd
Kevin Smith <kevin@kvnsmith.com>
2018-05-22 03:07:01 -0600

    please4

--- app.js
+++ app.js
@@ -1,48 +1,70 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var http = require('http'),
+    path = require('path'),
+    methods = require('methods'),
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    session = require('express-session'),
+    cors = require('cors'),
+    passport = require('passport'),
+    errorhandler = require('errorhandler'),
+    mongoose = require('mongoose');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var isProduction = process.env.NODE_ENV === 'production';
 
+// Create global app object
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.use(cors());
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
+// Normal express config defaults
+app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
 
-app.use('/', routes);
-app.use('/users', users);
+app.use(require('method-override')());
+app.use(express.static(__dirname + '/public'));
 
-// catch 404 and forward to error handler
+app.use(session({ secret: 'conduit', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false  }));
+
+if (!isProduction) {
+  app.use(errorhandler());
+}
+
+if(isProduction){
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+  mongoose.connect('mongodb://localhost/conduit');
+  mongoose.set('debug', true);
+}
+
+require('./models/User');
+require('./models/Article');
+require('./models/Comment');
+require('./config/passport');
+
+app.use(require('./routes'));
+
+/// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handlers
+/// error handlers
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
+if (!isProduction) {
   app.use(function(err, req, res, next) {
+    console.log(err.stack);
+
     res.status(err.status || 500);
-    res.render('error', {
+
+    res.json({'errors': {
       message: err.message,
       error: err
-    });
+    }});
   });
 }
 
@@ -50,11 +72,13 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.json({'errors': {
     message: err.message,
     error: {}
-  });
+  }});
 });
 
-
-module.exports = app;
+// finally, let's start our server...
+var server = app.listen( process.env.PORT || 3000, function(){
+  console.log('Listening on port ' + server.address().port);
+});
